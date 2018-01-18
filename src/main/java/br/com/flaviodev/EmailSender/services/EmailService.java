7a70ccc797b43cf0150ecc955fcb01d46391cdbd
flavio <fdsdev@gmail.com>
2018-01-18 15:06:11 -0200

    creation lib sender email

---
+++ src/main/java/br/com/flaviodev/EmailSender/services/EmailService.java
@@ -0,0 +1,29 @@
+package br.com.flaviodev.EmailSender.services;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	public void send(String name, String emailAddress, String subject, String message) {
+		try {
+			Email email = new SimpleEmail();
+			email.setHostName("smtp.googlemail.com");
+			email.setSmtpPort(465);
+			email.setAuthenticator(new DefaultAuthenticator("appflaviodev@gmail.com", "xxxxxx"));
+			email.setSSLOnConnect(true);
+
+			email.setFrom("appflaviodev@gmail.com");
+			email.setSubject(subject);
+			email.setMsg(message);
+			email.addTo(emailAddress);
+			email.send();
+
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}
+	}
+}
